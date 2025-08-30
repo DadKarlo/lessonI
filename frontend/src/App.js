39c6cf4980ec0fc7781dbe3b4fca719e10d4067e
PapaCarlo <dimon9608@bk.ru>
2025-08-30 22:28:16 +0300

    2-time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import BookForm from './components/BookForm/BookForm'
+import BookList from './components/BookList/BookList'
+import Filter from './components/Filter/Filter'
 import './App.css'
 
 function App() {
 	return (
 		<div className="app">
-			<h1>dfff</h1>
+			<h1>Book Library App</h1>
+			<main className="app-main">
+				<div className="app-left-column">
+					<BookForm />
+				</div>
+				<div className="app-right-column">
+					<Filter />
+					<BookList />
+				</div>
+			</main>
 		</div>
 	)
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { Profiler } from 'react'
+import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+	<Profiler store={store}>
+		<App />
+	</Profiler>
+)
