651b9503dd53338095862ac60de5bb1cf0b7f951
PapaCarlo <dimon9608@bk.ru>
2025-09-03 15:14:59 +0300

    5-time-Filter

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,13 +1,22 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { deleteBook } from '../../redux/books/actionCreators'
+import { selectTitleFilter } from '../../redux/slices/filterSlice'
 import './BookList.css'
 
 const BookList = () => {
 	const books = useSelector((state) => state.books)
+	const titleFilter = useSelector(selectTitleFilter)
 	const dispatch = useDispatch()
 	const handleDeleteBook = (id) => {
 		dispatch(deleteBook(id))
 	}
+
+	const filteredBooks = books.filter((book) => {
+		const matchesTitle = book.title
+			.toLowerCase()
+			.includes(titleFilter.toLowerCase())
+		return matchesTitle
+	})
 
 	return (
 		<div className="app-block book-list">
@@ -16,7 +25,7 @@ const BookList = () => {
 				<p>no alements...</p>
 			) : (
 				<ul>
-					{books.map((book, i) => (
+					{filteredBooks.map((book, i) => (
 						<li key={book.id}>
 							<div className="book-info">
 								{++i}. <strong>{book.title}</strong> and {book.author}
--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,9 +1,34 @@
 import './Filter.css'
+import { setTitleFilter, resetFilters } from '../../redux/slices/filterSlice'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Filter = () => {
+	const dispatch = useDispatch()
+	const titleFilter = useSelector((state) => state.filter.title)
+
+	const handleTitleFilterChenge = (e) => {
+		dispatch(setTitleFilter(e.target.value))
+	}
+
+	const handleResetFilters = () => {
+		dispatch(resetFilters())
+	}
+
 	return (
 		<div className="app-block filter">
-			<h2>Filter</h2>
+			<div className="filter-row">
+				<div className="filter-group">
+					<input
+						type="text"
+						value={titleFilter}
+						placeholder="filter..."
+						onChange={handleTitleFilterChenge}
+					/>
+				</div>
+				<button type="button" onClick={handleResetFilters}>
+					Reset filter
+				</button>
+			</div>
 		</div>
 	)
 }
